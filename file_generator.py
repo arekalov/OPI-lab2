9569e6d8b2b6332644c277fdd7c2fba0cd9a6ac0
arekalov <artyom.rekalov@gmail.com>
2025-02-22 18:02:50 +0300

    [fix] change add receiving home_path in gile_generator

--- file_generator.py
+++ file_generator.py
@@ -1,24 +1,23 @@
 import os
 import shutil
+import argparse
 
-def main():
-    HOME_PATH = "/Users/arekalov/Itmo/4/OPI/lab2/data"
-
+def main(home_path):
     # Remove the directory if it exists
-    if os.path.exists(HOME_PATH):
-        shutil.rmtree(HOME_PATH)
+    if os.path.exists(home_path):
+        shutil.rmtree(home_path)
 
     # Create the main directory
-    os.makedirs(HOME_PATH)
+    os.makedirs(home_path)
 
     for i in range(40):
-        current_dir_path = os.path.join(HOME_PATH, str(i))
+        current_dir_path = os.path.join(home_path, str(i))
         os.makedirs(current_dir_path)
         with open(os.path.join(current_dir_path, str(i)), 'w') as f:
             f.write(f"Data for commit {i}")
 
         if i != 0:
-            prev_dir_path = os.path.join(HOME_PATH, str(i - 1))
+            prev_dir_path = os.path.join(home_path, str(i - 1))
             for root, dirs, files in os.walk(prev_dir_path):
                 for file in files:
                     source = os.path.join(root, file)
@@ -28,4 +27,8 @@ def main():
                     shutil.copy2(source, target)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Generate directory structure for commits.")
+    parser.add_argument('home_path', type=str, help='The base path for creating the directory structure.')
+    
+    args = parser.parse_args()
+    main(args.home_path)
--- svn.sh
+++ svn.sh
@@ -1,201 +1,26 @@
-# SVN Commands definition
+#Variables
+REPO_URL="file://$(pwd)/svn/repo"
+COMMITS="$(pwd)/svn/data"
+CURRENT_USER="red"
 
-divider() {
-    echo "-------------"
-}
+#Functions definitions
 
-red() {
-    # Настройка пользователя RED
-    svn propset svn:author red . -m "Setting user to RED"
-    echo "Change user to RED"
-}
 
-blue() {
-    # Настройка пользователя BLUE
-    svn propset svn:author blue . -m "Setting user to BLUE"
-    echo "Change user to BLUE"
-}
 
-commit() {
-    if [ -z "$1" ]; then
-        echo "Error: Commit message required."
-        return 1
-    fi
 
-    # Добавление файлов и коммит
-    svn add . && svn commit -m "$1"
-    echo "Commit '$1' on $(basename $(svn info | awk -F': ' '/^URL/ {print $2}'))"
-    divider
-}
 
-create_branch() {
-    if [ -z "$1" ]; then
-        echo "Error: Branch name required."
-        return 1
-    fi
 
-    # Создание ветки
-    svn copy ^/trunk ^/branches/"$1" -m "Creating branch $1"
-    svn switch ^/branches/"$1"
-    echo "Create '$1' and checkout to it"
-}
+#Init
+rm -rf svn
+mkdir svn
+cd svn
 
-merge() {
-    if [ -z "$1" ]; then
-        echo "Error: Branch name required."
-        return 1
-    fi
+svnadmin create repo
+svn mkdir $REPO_URL/trunk $REPO_URL/branches -m "Init file structure" --username $CURRENT_USER
 
-    # Проверка существования ветки
-    if svn info ^/branches/"$1" >/dev/null 2>&1; then
-        # Слияние веток
-        svn merge ^/branches/"$1" ^/trunk -m "Merge branch $1 to trunk"
-        echo "Merged $(basename $(svn info | awk -F': ' '/^URL/ {print $2}')) to $1"
-    else
-        echo "The branch \"$1\" does not exist."
-    fi
-}
+#Commits files generating
+python3 ../file_generator.py $(pwd)/data
 
-# Init
-svnadmin create svn_repo
-svn mkdir file://$(pwd)/svn_repo/trunk -m "Creating trunk"
-svn mkdir file://$(pwd)/svn_repo/branches -m "Creating branches"
-svn checkout file://$(pwd)/svn_repo/trunk svn_working_copy
-cd svn_working_copy
-
-red
-create_branch branch0
-commit 0
-
-create_branch branch1
-commit 1
-
-create_branch branch2
-commit 2
-
-svn switch ^/branches/branch1
-commit 3
-
-svn switch ^/branches/branch0
-commit 4
-
-create_branch branch5
-commit 5
-
-create_branch branch6
-commit 6
-
-svn switch ^/branches/branch0
-commit 7
-
-svn switch ^/branches/branch5
-commit 8
-
-blue
-create_branch branch9
-commit 9
-
-red 
-create_branch branch10
-commit 10
-
-blue
-create_branch branch11
-commit 11
-
-commit 12
-
-red 
-svn switch ^/branches/branch2
-commit 13
-
-svn switch ^/branches/branch6
-commit 14
-
-commit 15
-
-commit 16
-
-svn switch ^/branches/branch10
-commit 17
-
-svn switch ^/branches/branch11
-merge branch10
-commit 18
-
-svn switch ^/branches/branch6
-merge branch11
-commit 19
-
-svn switch ^/branches/branch0
-merge branch6
-commit 20
-
-svn switch ^/branches/branch5
-commit 21
-
-svn switch ^/branches/branch1
-commit 22
-
-blue
-create_branch branch23
-merge branch1
-commit 23
-
-commit 24
-
-red
-svn switch ^/branches/branch5
-commit 25
-
-blue
-create_branch branch26
-commit 26
-
-red 
-svn switch ^/branches/branch2
-commit 27
-
-svn switch ^/branches/branch0
-commit 28
-
-svn switch ^/branches/branch2
-commit 29
-
-commit 30
-
-svn switch ^/branches/branch5
-merge branch2
-commit 31
-
-svn switch ^/branches/branch0
-commit 32
-
-blue
-svn switch ^/branches/branch23
-commit 33
-
-red
-svn switch ^/branches/branch5
-commit 34
-
-blue
-create_branch branch26
-merge branch5
-commit 35
-
-commit 36
-
-svn switch ^/branches/branch23
-merge branch26
-commit 37
-
-svn switch ^/branches/branch9
-merge branch23
-commit 38
-
-red
-svn switch ^/branches/branch0
-merge branch9
-commit 39
-
+#Create working directory
+svn checkout $REPO_URL/trunk working_copy
+cd working_copy
